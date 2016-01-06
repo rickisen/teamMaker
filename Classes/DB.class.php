259afdcd0bd5bfcfaecd6c9c62e1293a69ce5b36
rickisen <rickard.spam@gmail.com>
2016-01-06 18:09:23 +0100

    first working version

--- Classes/DB.class.php
+++ Classes/DB.class.php
@@ -7,7 +7,7 @@ class DB{
 
 	public static function getInstance(){
 		if(!self::$instance){
-      $config = parse_ini_file('../Assets/mysqliConfig.ini');
+                $config = parse_ini_file('Assets/mysqliConfig.ini');
 			self::$instance = new mysqli($config['host'], $config['user'], $config['password'], $config['database']);
 			return self::$instance;
 		}else{
--- teamMaker.php
+++ teamMaker.php
@@ -3,13 +3,20 @@
 
 require_once "Classes/DB.class.php";
 
-// loop through all our specifity levels 
-for ($i = 0 ; $i != 4; $i++){
+// testing only
+/* insertTestUsers(); */
+
+// loop through all our specifity levels, be specific first
+for ($i = 4 ; $i != 0; $i--){
   // get this specificity level's teams
   $teams = groupPlayers($i);
-  
+
+  // testing only 
+  print_r($teams);
+
   // move the players into their lobbies
-  movePlayers($teams);
+  if (!empty($teams))
+    movePlayers($teams);
 }
 
 // Grouping function, takes 1 parameter, the level of specificity to group players on.
@@ -36,11 +43,11 @@ function groupPlayers($specificity){
   // the generated Query for getting relevant playerID's, only get groups that contain 5 or more players, as they are 
   // the only groups that could hold interesting players this run, and order them by the the time they where added to the pool
   $qGetPlayerGroups = '
-    SELECT count(user.steam_id) AS size, 
-           GROUP_CONCAT(user.steam_id ORDER BY started_looking ASC SEPERATOR ",") AS users
+    SELECT  count(user.steam_id) AS size, 
+            GROUP_CONCAT(user.steam_id ORDER BY started_looking ASC ) AS users
 
-    FROM   players_looking_for_lobby LEFT JOIN user 
-             ON players_looking_for_lobby.steam_id = user.steam_id;
+    FROM   player_looking_for_lobby LEFT JOIN user 
+             ON player_looking_for_lobby.steam_id = user.steam_id 
     
     GROUP BY '.$activeAttributes.'
     HAVING size > 4
@@ -57,15 +64,18 @@ function groupPlayers($specificity){
       // the query should reutrn a users collumn, that consits of the 
       // users in that group's steamIDs sepperated by a ','
       // Explode them into an array
-      $users = explode($group['users'], ',');
-
-      // To be used as a index for accesseing values in the exploded array
-      $player = 0 ;
+      $users = explode(',', $group['users'] );
     
       // put the users in this player group into their teams in the return-array, 
       // and ignore the last users who don't fit into a team, (they will probably fit into a team next run)
       for ( $i = 0 ; $i != $ammountOfTeamsInGroup ; $i++ ) {
-        $ret[] = [$users[$player++], $users[$player++], $users[$player++], $users[$player++], $users[$player++] ];
+        $playerOne   = 0 + $i * 5;
+        $playerTwo   = 1 + $i * 5;
+        $playerThree = 2 + $i * 5;
+        $playerFour  = 3 + $i * 5;
+        $playerFive  = 4 + $i * 5;
+
+        $ret[] = [ $users[$playerOne], $users[$playerTwo], $users[$playerThree], $users[$playerFour], $users[$playerFive] ];
       }
     }
     // TODO if we are on a very low specifity level, (aka we are not very picky about who we end up with) Only make a team if all players have waited a long time
@@ -81,13 +91,13 @@ function movePlayers($teams){
   //  loop through each team,
   foreach ($teams as $team){
     // generate a uniq_id for this lobby. 
-    $lobbyId = uinqid();
+    $lobbyId = uniqid();
     // loop through each player in this team
-    foreach ($teams as $player){// add this player into the lobby table
+    foreach ($team as $player){// add this player into the lobby table
       // query to load a player into the lobby table
       $qInsertPlayerIntoLobby = '
-        INSERT INTO lobby (steam_id, lobby_id,)
-        VALUES ( "'.$player.'", "'.$lobbyId.'", )
+        INSERT INTO lobby (steam_id, lobby_id)
+        VALUES ( "'.$player.'", "'.$lobbyId.'" )
       ';
 
       if ( ! $result = $database->query($qInsertPlayerIntoLobby)){
@@ -97,7 +107,7 @@ function movePlayers($teams){
       // remove it from players_looking_for_lobby
       // query to remove a player from the players_looking_for_lobby table
       $qDeleteUserFromLooking = '
-        DELETE FROM players_looking_for_lobby WHERE steam_id = '.$player.'
+        DELETE FROM player_looking_for_lobby WHERE steam_id = '.$player.'
       ';
 
       if ( ! $result = $database->query($qDeleteUserFromLooking)){
@@ -105,4 +115,57 @@ function movePlayers($teams){
       }
     }
   }
+}
+
+// testing only 
+function insertTestUsers(){
+  $database = DB::getInstance() ;
+
+  $qInsertTesters = '
+    INSERT INTO player_looking_for_lobby (steam_id)
+    VALUES 
+        ( "76561198119133793" ),
+        ( "76561197961825525" ),
+        ( "76561197960964569" ),
+        ( "76561198010808320" ),
+        ( "76561197981707560" ),
+        ( "76561198166994467" ),
+        ( "76561197963624696" ),
+        ( "76561198147424033" ),
+        ( "76561198217983724" ),
+        ( "76561198071773272" ),
+        ( "76561198193558745" ),
+        ( "76561198177806779" ),
+        ( "76561198185709861" ),
+        ( "76561198127537132" ),
+        ( "76561198170008233" ),
+        ( "76561198079853120" ),
+        ( "76561198023969636" ),
+        ( "76561198165835665" ),
+        ( "76561198121413467" ),
+        ( "76561197964408980" ),
+        ( "76561198004464720" ),
+        ( "76561197961109569" ),
+        ( "76561197997562376" ),
+        ( "76561197967790391" ),
+        ( "76561197960325993" ),
+        ( "76561197979337863" ),
+        ( "76561197967090145" ),
+        ( "76561198090730839" ),
+        ( "76561197961395244" ),
+        ( "76561197961049556" ),
+        ( "76561198154156138" ),
+        ( "76561198039027545" ),
+        ( "76561198017223611" ),
+        ( "76561197960670668" ),
+        ( "76561197960423662" ),
+        ( "76561197960329953" ),
+        ( "76561197992700833" ),
+        ( "76561198004727120" ),
+        ( "76561197978857993" ),
+        ( "76561198083924003" ),
+        ( "76561197960815531" )
+  ';
+
+  $database->query($qInsertTesters);
 }
