5f45be5d8431a1729987f1dd7322f60e8382f5cf
rickisen <rickard.spam@gmail.com>
2016-01-13 14:33:15 +0100

    indent fixez

--- Classes/LobbyMaker.class.php
+++ Classes/LobbyMaker.class.php
@@ -70,13 +70,13 @@ class LobbyMaker {
   static function movePlayer($player, $lobbyId, $quality, $isLeader = FALSE) {
     $database = DB::getInstance() ;
 
-                // convert from bool to int
-                if ($isLeader)
-                  $isLeader = 1;
-                else
-                  $isLeader = 0;
-                
-                // move player into the lobby table
+    // convert from bool to int
+    if ($isLeader)
+      $isLeader = 1;
+    else
+      $isLeader = 0;
+    
+    // move player into the lobby table
     $qMoveMember = '
       INSERT INTO lobby (lobby_id, steam_id, quality, is_leader)
       VALUES ("'.$lobbyId.'","'.$player.'","'.$quality.'", '.$isLeader.');
@@ -114,18 +114,18 @@ class LobbyMaker {
     ';
 
     // query the db and put all the losers currently in there into new lobbies
-                if( $result = $database->query($qLevelZero)) {
-                  while( $row = $result->fetch_assoc()){
-                    // add the current user into the newest lobby
-                    $lobbies->addMember($row['steam_id'], 0);
-                  }
-                } 
+    if( $result = $database->query($qLevelZero)) {
+      while( $row = $result->fetch_assoc()){
+        // add the current user into the newest lobby
+        $lobbies->addMember($row['steam_id'], 0);
+      }
+    } 
 
-                if ($error = $database->error){
-                  echo "something wrong with levelZero: ".$error;
-                }
+    if ($error = $database->error){
+      echo "something wrong with levelZero: ".$error;
+    }
 
-                self::movePlayers(); // is usually run by HandleGroupResults
+    self::movePlayers(); // is usually run by HandleGroupResults
   }
 
   static function levelOne(){
@@ -143,14 +143,14 @@ class LobbyMaker {
         HAVING size >= 5
     ';
     
-                // query the db, and if we got some results, handle the properly
-                if( $result = $database->query($qLevelOne)){
-                  self::HandleGroupResults($result, 1);
-                } 
+    // query the db, and if we got some results, handle the properly
+    if( $result = $database->query($qLevelOne)){
+      self::HandleGroupResults($result, 1);
+    } 
 
-                if ($error = $database->error){
-                  echo "something wrong with levelOne: ".$error;
-                }
+    if ($error = $database->error){
+      echo "something wrong with levelOne: ".$error;
+    }
   }
 
   static function levelTwo(){
@@ -169,100 +169,100 @@ class LobbyMaker {
         HAVING size >= 5
     ';
     
-                // query the db, and if we got some results, handle them properly
-                if( $result = $database->query($qLevelTwo)){
-                  self::HandleGroupResults($result, 2);
-                } 
+    // query the db, and if we got some results, handle them properly
+    if( $result = $database->query($qLevelTwo)){
+      self::HandleGroupResults($result, 2);
+    } 
 
-                if ($error = $database->error){
-                  echo "something wrong with levelTwo: ".$error;
-                }
+    if ($error = $database->error){
+      echo "something wrong with levelTwo: ".$error;
+    }
   }
 
   static function levelThree(){
     $lobbies  = self::getLobbyHolder();
     $database = DB::getInstance() ;
 
-                // Queries that gets all languages that have speakers in the db
-                $qGetAllPriLangs = ' 
+    // Queries that gets all languages that have speakers in the db
+    $qGetAllPriLangs = ' 
           SELECT DISTINCT primary_language 
           FROM user 
           WHERE primary_language IS NOT NULL 
             AND primary_language != ""';
 
-                $qGetAllSecLangs = ' 
+    $qGetAllSecLangs = ' 
           SELECT DISTINCT secondary_language 
           FROM user 
           WHERE secondary_language IS NOT NULL 
             AND secondary_language != ""';
 
-                // this will come to hold all the spoken languages, both primary and secondary
-                $langs = array(); 
+    // this will come to hold all the spoken languages, both primary and secondary
+    $langs = array(); 
 
-                // add all primary languages into langs
-                $priLangResult = $database->query($qGetAllPriLangs);
-                while ($row = $priLangResult->fetch_assoc())
-                  $langs[] = $row['primary_language'];
+    // add all primary languages into langs
+    $priLangResult = $database->query($qGetAllPriLangs);
+    while ($row = $priLangResult->fetch_assoc())
+      $langs[] = $row['primary_language'];
 
-                // and secondaries
-                $secLangResult = $database->query($qGetAllSecLangs);
-                while ($row = $secLangResult->fetch_assoc())
-                  $langs[] = $row['secondary_language'];
+    // and secondaries
+    $secLangResult = $database->query($qGetAllSecLangs);
+    while ($row = $secLangResult->fetch_assoc())
+      $langs[] = $row['secondary_language'];
 
-    
-                // this makes sure that there is only one copy 
-                // of every spoken language in this array
-                $langs = array_unique($langs);
 
-                // for every spoken language we make a new group query
-                foreach ($langs as $lang){
-                      $qLevelThree = '
-                          SELECT  count(user.steam_id) AS size, 
-                                  GROUP_CONCAT(user.steam_id ORDER BY started_looking ASC) AS users
+    // this makes sure that there is only one copy 
+    // of every spoken language in this array
+    $langs = array_unique($langs);
 
-                          FROM   player_looking_for_lobby LEFT JOIN user 
-                                   ON player_looking_for_lobby.steam_id = user.steam_id 
+    // for every spoken language we make a new group query
+    foreach ($langs as $lang){
+      $qLevelThree = '
+          SELECT  count(user.steam_id) AS size, 
+                  GROUP_CONCAT(user.steam_id ORDER BY started_looking ASC) AS users
 
-                          WHERE  primary_language   = "'.$lang.'"
-                             OR  secondary_language = "'.$lang.'"
-                             AND started_looking < (UNIX_TIMESTAMP() - 100)
+          FROM   player_looking_for_lobby LEFT JOIN user 
+                   ON player_looking_for_lobby.steam_id = user.steam_id 
 
-                          GROUP BY rank, age_group
-                          HAVING size >= 5
-                      ';
-                      
-                      // query the db, and if we got some results, handle them properly
-                      if( $result = $database->query($qLevelThree)){
-      if ( $numberOfRows = $result->num_rows > 0 ){
-        echo "found ".$numberOfRows." $lang speaking player groups \n";
-        self::HandleGroupResults($result, 3);
+          WHERE  primary_language   = "'.$lang.'"
+             OR  secondary_language = "'.$lang.'"
+             AND started_looking < (UNIX_TIMESTAMP() - 100)
+
+          GROUP BY rank, age_group
+          HAVING size >= 5
+      ';
+      
+      // query the db, and if we got some results, handle them properly
+      if( $result = $database->query($qLevelThree)){
+        if ( $numberOfRows = $result->num_rows > 0 ){
+          echo "found ".$numberOfRows." $lang speaking player groups \n";
+          self::HandleGroupResults($result, 3);
+        }
+      } 
+
+      if ($error = $database->error){
+        echo "something wrong with levelThree on language $lang: ".$error;
       }
-                      } 
-
-                      if ($error = $database->error){
-                        echo "something wrong with levelThree on language $lang: ".$error;
-                      }
-                }
+    }
   }
 
-        static function HandleGroupResults($GResult, $qualityLevel){
+  static function HandleGroupResults($GResult, $qualityLevel){
     $lobbies  = self::getLobbyHolder();
-        while ($group = $GResult->fetch_assoc()) {
-            $users = explode(',', $group['users'] );
-                        $ammountOfTeamsInGroup = ($group['size'] - ( $group['size'] % 5 )) / 5  ;
+    while ($group = $GResult->fetch_assoc()) {
+      $users = explode(',', $group['users'] );
+      $ammountOfTeamsInGroup = ($group['size'] - ( $group['size'] % 5 )) / 5  ;
 
-                        // loop like this so that we'll make as many teams as possible 
-                        // out of this group of users, and leave the people who didn't 
-                        // fit into a team in the pool of users who are looking for teams
-                        // They'll surely find mates on the next run
-                        $userCounter = 0 ; 
-                        for ($i = 0; $i != $ammountOfTeamsInGroup ; $i++){
-                          $lobbies->addLobby($qualityLevel); 
-                          for ($j = 0 ; $j != 5 ; $j++) // add exactly 5 users into a team
-                            $lobbies->lastLobby()->addMember($users[$userCounter++]);
-                        }
+      // loop like this so that we'll make as many teams as possible 
+      // out of this group of users, and leave the people who didn't 
+      // fit into a team in the pool of users who are looking for teams
+      // They'll surely find mates on the next run
+      $userCounter = 0 ; 
+      for ($i = 0; $i != $ammountOfTeamsInGroup ; $i++){
+        $lobbies->addLobby($qualityLevel); 
+        for ($j = 0 ; $j != 5 ; $j++) // add exactly 5 users into a team
+          $lobbies->lastLobby()->addMember($users[$userCounter++]);
+      }
     }
-                // move the players now so that we don't try to lobby people twice on level 3
-                self::movePlayers();
-        }
+    // move the players now so that we don't try to lobby people twice on level 3
+    self::movePlayers();
+  }
 }
